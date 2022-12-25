d9c6087d226ab5829dd8b0de2fd373bddb97e618
sergioalsantos <116590467+sergioalsantos@users.noreply.github.com>
2022-12-25 19:02:51 -0300

    Add files via upload

---
+++ Exercicio 15-11-2022.py
@@ -0,0 +1,13 @@
+numero1 = int(input('Digite o valor A: '))
+numero2 = int(input('Digite o valor B: '))
+numero3 = int(input('Digite o valor C: '))
+numero4 = int(input('Digite o valor D: '))
+
+resultado_soma = numero1 + numero2
+
+resultado_soma1 = numero3 + numero4
+
+resultado_adicao = resultado_soma - resultado_soma1
+
+print(" Resultado de A e B =", resultado_soma,  " Resultado de C e D =", resultado_soma1,
+      "a divisão do resultado entre eles foram", resultado_adicao)
